4aca41e2e1793370b2aa5b0f9dcebe90053fc167
chuckderosier <chuckderosier2018@gmail.com>
2019-05-01 14:56:45 -0400

    fixed errors but not working

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ const InsultStyles = styled.div`
 `
 
 class App extends Component {
-  render() {
 
-    // const adjective1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"]
+      // const adjective1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"]
     // const adjective2 = ["base-court", "bat-fowling", "beff-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"]
     // const noun = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pidgeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
 
@@ -24,27 +23,28 @@ class App extends Component {
       noun: ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pidgeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
     }
 
-    function rng() {
-      Math.floor((Math.random() * 48))
-    }
+    // function rng() {
+    //   Math.floor((Math.random() * 48))
+    // }
 
-    function makeQuote(event) {
+    onClick = function makeQuote(event) {
       event.preventDefault()
-      let adj1 = rng()
-      let adj2 = rng()
-      let noun3 = rng()
+      let adj1 = Math.floor((Math.random() * 48))
+      let adj2 = Math.floor((Math.random() * 48))
+      let noun3 = Math.floor((Math.random() * 48))
       document.getElementById("adj1").innerHTML = this.state.adjective1[adj1]
       document.getElementById("adj2").innerHTML = this.state.adjective2[adj2]
       document.getElementById("noun3").innerHTML = this.state.noun[noun3]
     }
 
+  render() {
     return (
       <InsultStyles>
         <h1>Randomly Create A Shakespearean Insult</h1>
         <p id="quote">Thou <span id="adj1"></span><span id="adj2"></span></p><br />
         <p id="noun3"></p>
         <p>47</p>
-        <button onclick="makeQuote()">Clicketh Here</button>
+        <button type="onClick">Clicketh Here</button>
       </InsultStyles>
     )
   }
