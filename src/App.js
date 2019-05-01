049195c8f1d858363959f907cfa847b480935b1f
chuckderosier <chuckderosier2018@gmail.com>
2019-05-01 14:17:19 -0400

    added button, delete css files

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import styled from 'styled-components'
 const InsultStyles = styled.div`
     font-family: 'Pirata One', cursive;
     letter-spacing: .2em;
+    -webkit-text-stroke-width: .01em;
+    -webkit-text-stroke-color: gray;
     text-align: center;
+    background-color: black;
+    color: white;
 `
 
 const adjective1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"]
 const adjective2 = ["base-court", "bat-fowling", "beff-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"]
 const noun = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pidgeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
 
-let ad1 = Math.floor((Math.random() * 48))
+function rng() {
+  Math.floor((Math.random() * 48))
+}
 
 class App extends Component {
   render() {
@@ -19,6 +25,7 @@ class App extends Component {
       <InsultStyles>
         <h1>Randomly Create A Shakespearean Insult</h1>
         <p>47</p>
+        <button onClick="">Clicketh Here</button>
       </InsultStyles>
     )
   }
