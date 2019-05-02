8ca22d49e7ae3796133f910723d45e7bb20de6cb
chuckderosier <chuckderosier2018@gmail.com>
2019-05-02 18:00:44 -0400

    more css and favicon

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ const InsultStyles = styled.div`
     text-align: center;
     background-color: black;
     color: white;
+    h1 {
+      font-size: 1.8em;
+      margin: 0 auto;
+      padding: .2em;
+    }
+    button {
+      font-family: 'Fondamento', cursive;
+      font-size: 1em;
+      margin: .1em auto;
+      padding: .2em .3em;
+    }
     #quote-container {
       display: flex;
     }
@@ -18,7 +29,6 @@ const InsultStyles = styled.div`
 const adjective1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"]
 const adjective2 = ["base-court", "bat-fowling", "beff-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"]
 const noun = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pidgeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
-
 
 class App extends Component {
 
@@ -36,9 +46,10 @@ class App extends Component {
     return (
       <InsultStyles>
         <h1>Randomly Create A Shakespearean Insult</h1>
+        <button onClick={this.makeQuote}>Clicketh Here</button>
         <div id="quote-container">
           <div>
-            <img src="https://i.imgur.com/tm7Esfu.png" alt="shakespeare portrait" />
+            <img src="https://i.imgur.com/7JBOfEX.png" alt="shakespeare portrait" />
           </div>
           <div id="quote">
             <p>Thou</p>
@@ -47,7 +58,6 @@ class App extends Component {
             <p id="noun3"></p>
           </div>
         </div>
-        <button onClick={this.makeQuote}>Clicketh Here</button>
       </InsultStyles>
     )
   }
