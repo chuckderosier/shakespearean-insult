3f401838a922011dce39cc70ff14c643bc659a1d
chuckderosier <chuckderosier2018@gmail.com>
2019-05-08 14:14:10 -0400

    exclamation point

--- src/App.js
+++ src/App.js
@@ -57,6 +57,9 @@ const InsultStyles = styled.div`
       font-size: 2em;
       margin: .1em auto;
     }
+    span {
+      color: white;
+    }
   @media (min-width: 510px) and (max-width: 767px) {
     h1 {
       font-size: 1.4em;
@@ -84,7 +87,7 @@ const InsultStyles = styled.div`
 const thou = ["Thou"]
 const adjective1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"]
 const adjective2 = ["base-court", "bat-fowling", "beff-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"]
-const noun = ["apple-john !!", "baggage !!", "barnacle !!", "bladder !!", "boar-pig !!", "bugbear !!", "bum-bailey !!", "canker-blossom !!", "clack-dish !!", "coxcomb !!", "codpiece !!", "death-token !!", "dewberry !!", "flap-dragon !!", "flax-wench !!", "flirt-gill !!", "foot-licker !!", "fustilarian !!", "giglet !!", "gudgeon !!", "haggard !!", "harpy !!", "hedge-pig !!", "horn-beast !!", "hugger-mugger !!", "joithead !!", "lewdster !!", "lout !!", "maggot-pie !!", "malt-worm !!", "mammet !!", "measle !!", "minnow !!", "miscreant !!", "moldwarp !!", "mumble-news !!", "nut-hook !!", "pidgeon-egg !!", "pignut !!", "puttock !!", "pumpion !!", "ratsbane !!", "scut !!", "skainsmate !!", "strumpet !!", "varlot !!", "vassal !!", "whey-face !!", "wagtail !!"]
+const noun = ["apple-john!", "baggage!", "barnacle!", "bladder!", "boar-pig!", "bugbear!", "bum-bailey!", "canker-blossom!", "clack-dish!", "coxcomb!", "codpiece!", "death-token!", "dewberry!", "flap-dragon!", "flax-wench!", "flirt-gill!", "foot-licker!", "fustilarian!", "giglet!", "gudgeon!", "haggard!", "harpy!", "hedge-pig!", "horn-beast!", "hugger-mugger!", "joithead!", "lewdster!", "lout!", "maggot-pie!", "malt-worm!", "mammet!", "measle!", "minnow!", "miscreant!", "moldwarp!", "mumble-news!", "nut-hook!", "pidgeon-egg!", "pignut!", "puttock!", "pumpion!", "ratsbane!", "scut!", "skainsmate!", "strumpet!", "varlot!", "vassal!", "whey-face!", "wagtail!"]
 
 class App extends Component {
 
@@ -109,10 +112,10 @@ class App extends Component {
             <img src="https://i.imgur.com/7JBOfEX.png" alt="shakespeare portrait" />
           </div>
           <div id="quote">
-            <p id="thou"></p>
+            <p id="thou"><span>.</span></p>
             <p id="adj1"></p>
             <p id="adj2"></p>
-            <p id="noun3"><span className="ex"></span></p>
+            <p id="noun3"><span>.</span></p>
           </div>
         </div>
       </InsultStyles>
